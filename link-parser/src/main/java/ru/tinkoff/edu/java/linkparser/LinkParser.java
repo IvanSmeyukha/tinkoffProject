ff95942cefc5d155dbdcec36f90f71cebd378d0f
BAHOXA <ivan.smeiukha@yandex.ru>
2023-03-20 00:57:50 +0300

    init

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/LinkParser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/LinkParser.java
@@ -1,6 +1,7 @@
 package ru.tinkoff.edu.java.linkparser;
 
 public class LinkParser {
+
     public static String parseLink(String link){
         LinkHandler gitHubLinkHandler = new GitHubLinkHandler(null);
         LinkHandler stackOverflowLinkHandler = new StackOverflowLinkHandler(gitHubLinkHandler);
