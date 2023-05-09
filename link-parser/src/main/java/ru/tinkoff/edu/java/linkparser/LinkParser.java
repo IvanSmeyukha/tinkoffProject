96ef26e6fa1c4b4f64529e82c2a3011e34a12809
BAHOXA <ivan.smeiukha@yandex.ru>
2023-05-09 23:12:07 +0300

    fix style

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/LinkParser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/LinkParser.java
@@ -4,7 +4,7 @@ public class LinkParser {
     private static final LinkHandler gitHubLinkHandler = new GitHubLinkHandler(null);
     private static final LinkHandler stackOverflowLinkHandler = new StackOverflowLinkHandler(gitHubLinkHandler);
 
-    public static LinkParserResponse parseLink(String link){
+    public static LinkParserResponse parseLink(String link) {
         return stackOverflowLinkHandler.parseLink(link);
     }
 }
--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/Main.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println(LinkParser.parseLink("https://github.com/sanyarnd/tinkoff-java-course-2022/"));
         System.out.println(LinkParser.parseLink("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c"));
         System.out.println(LinkParser.parseLink("https://stackoverflow.com/search?q=unsupported%20link"));
-        System.out.println(LinkParser.parseLink("https://stackoverflow.com/questions/16420289999/what-is-the-operator-in-c"));
+        System.out.println(LinkParser.parseLink(
+            "https://stackoverflow.com/questions/16420289999/what-is-the-operator-in-c"));
     }
 }
