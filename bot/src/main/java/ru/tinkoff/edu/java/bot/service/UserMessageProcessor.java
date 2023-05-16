d425ebd76e15d4b82a6153196d5b39b5e60abac4
BAHOXA <ivan.smeiukha@yandex.ru>
2023-05-16 21:26:29 +0300

    10.2 Practice

--- bot/src/main/java/ru/tinkoff/edu/java/bot/service/UserMessageProcessor.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/service/UserMessageProcessor.java
@@ -3,6 +3,8 @@ package ru.tinkoff.edu.java.bot.service;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import java.util.List;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.tinkoff.edu.java.bot.service.command.Command;
@@ -11,10 +13,12 @@ import ru.tinkoff.edu.java.bot.service.command.Command;
 @RequiredArgsConstructor
 public class UserMessageProcessor {
     private final List<Command> commands;
+    private final Counter counter = Metrics.counter("bot_messages_processed_total");
     private static final String UNSUPPORTED_COMMAND_MESSAGE =
         "Я не знаю такую команду. Для вывода списка всех комманд используйте /help";
 
     public SendMessage process(Update update) {
+        counter.increment();
         return commands.stream()
             .filter(command -> command.supports(update))
             .findFirst()
